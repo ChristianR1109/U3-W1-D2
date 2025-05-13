47869af60ef433d35d37d9b92217305a2b921fc1
Christian Ragonese <christianragonese1313@gmail.com>
2025-05-13 17:27:25 +0200

    fix

--- new-app/src/components/AllTheBooks.jsx
+++ new-app/src/components/AllTheBooks.jsx
@@ -2,24 +2,27 @@ import { Container, Col, Image, Row } from "react-bootstrap";
 import books from "../data/books.json";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-const AllTheBooks = function () {
-  return (
-    <Container fluid="xxl">
-      <Row xs={2} md={3} lg={4} className="m-0">
-        {books.map((book) => (
-          <Col key={book.asin}>
-            <Card style={{ height: "99%" }}>
-              <Card.Img src={book.img} />
-              <Card.Body>
-                <Card.Title>{book.title}</Card.Title>
-                <Card.Text>{book.price}€</Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    </Container>
-  );
-};
+import { Component } from "react";
+class AllTheBooks extends Component {
+  render() {
+    return (
+      <Container fluid="xxl">
+        <Row xs={2} md={3} lg={4} className="m-0">
+          {books.map((book) => (
+            <Col key={book.asin}>
+              <Card style={{ height: "99%" }}>
+                <Card.Img src={book.img} />
+                <Card.Body>
+                  <Card.Title>{book.title}</Card.Title>
+                  <Card.Text>{book.price}€</Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    );
+  }
+}
 export default AllTheBooks;
